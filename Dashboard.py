1b5105fc554516ce2e498a90838d09a2025e817e
iyfyao <90681327+iyfyao@users.noreply.github.com>
2022-04-01 16:15:11 +0200

    Delete Dashboard.py

--- Dashboard.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-df = pd.read_csv('data_sources/time_series_covid19_confirmed_global.csv')
-print(df.iloc[0,700:])
-df.iloc[0,700:].plot()
-plt.show()
