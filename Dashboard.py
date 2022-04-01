29dad1ecf9b59af7b177316bd8f880533965a063
miky <maikel.Bastawrous@cri-paris.org>
2022-04-01 16:42:47 +0200

    final streamlit

--- Dashboard.py
+++ Dashboard.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv('data_sources/time_series_covid19_confirmed_global.csv')
+df = pd.read_csv('time_series_covid19_confirmed_global.csv')
 print(df.iloc[0,700:])
 df.iloc[0,700:].plot()
-plt.show()
+fig = plt.figure()
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import streamlit as st 
+import streamlit as st
 
 df = pd.read_csv('time_series_covid19_confirmed_global.csv')
 df
 #print(df.iloc[0,700:])
-#df.iloc[0,700:].plot()
-#plt.show()
-# st.write(df)
+fig = plt.figure()
+df.iloc[0,700:].plot()
+st.pyplot(fig)
+st.balloons()
