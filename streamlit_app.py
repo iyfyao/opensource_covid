be4a50dd1e452eced322a36b35dfdb847464a22a
MrMazagngy <90629390+MrMazagngy@users.noreply.github.com>
2022-04-01 15:32:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st 
 
-df = pd.read_csv('data_sources\time_series_covid19_confirmed_global.csv')
+df = pd.read_csv('time_series_covid19_confirmed_global.csv')
 df
 #print(df.iloc[0,700:])
 #df.iloc[0,700:].plot()
