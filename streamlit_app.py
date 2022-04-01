4ddd86fec7778872c264f2a196572ddb57442f28
iyfyao <90681327+iyfyao@users.noreply.github.com>
2022-04-01 16:40:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,15 @@ df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
+
 st.title('COVID Dashboard')
 df
-st.write(df)
+
+f1 = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+
+f1.head()
+
+st.write(f1.head())
+
+datagraph=f1['total_cases']
+st.line_chart(datagraph)
