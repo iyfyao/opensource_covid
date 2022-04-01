4f3530cc058fc2e1ac2604be7cdb3baac0f50b45
iyfyao <90681327+iyfyao@users.noreply.github.com>
2022-04-01 16:06:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import streamlit as st 
 
 df = pd.read_csv('time_series_covid19_confirmed_global.csv')
-#df
-df.iloc[0,700:].plot()
-fig = plt.figure()
-st.pyplot(fig)
+df
+#df.iloc[0,700:].plot()
+#fig = plt.figure()
+#st.pyplot(fig)
