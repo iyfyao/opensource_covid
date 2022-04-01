1d9315e44b4947c4faff90edeaf5b8b379c11e6a
MrMazagngy <90629390+MrMazagngy@users.noreply.github.com>
2022-04-01 16:07:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st 
-
+@st.cache
 df = pd.read_csv('time_series_covid19_confirmed_global.csv')
 df
 #df.iloc[0,700:].plot()
 #fig = plt.figure()
 #st.pyplot(fig)
+st.balloons()
