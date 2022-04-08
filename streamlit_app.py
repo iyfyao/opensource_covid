13b088489b06ef0bf5e28b093355bf6c257d28fa
iyfyao <90681327+iyfyao@users.noreply.github.com>
2022-04-08 11:35:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,15 @@ import numpy as np
 import streamlit as st
 
 pd.options.display.max_columns = None
-df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv') #new dataset
+url_to_data = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
+@st.cache
+def load_data():
+   data = pd.read_csv(url_to_data)
+   return data
+
+df = load_data()
+
+
 df['MA'] = df['total_cases'].rolling(window=7).mean() #7day Moving Average
 df_1 = df.groupby('date')['total_cases'].sum().reset_index() #Aggregate by date
 
